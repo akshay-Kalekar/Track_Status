7732e6bf0e000df90aa1990cee96a2a09b9f905f
Akshay <akalekar2003@gmail.com>
2024-09-18 19:58:30 +0530

    Updated Records

--- pages/1_Filter_Records.py
+++ pages/1_Filter_Records.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Filter Records", page_icon="🙀", layout="wide")
 st.title("🎓 List of Enrolled Students")
 
 # Read the CSV file
-df = pd.read_csv('./BTechData2.csv', na_filter=False)
+df = pd.read_csv('./data/22.csv', na_filter=False)
 
 # Display the full dataframe with hidden index
 st.dataframe(df, use_container_width=True, hide_index=True)
